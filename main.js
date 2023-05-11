1f22ea16b7399e69fd1e3333de3d04341fee2e96
Singhal <2270507@cognizant.com>
2023-05-11 13:19:46 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,31 @@
+let menu=document.querySelector('.menu-icon');
+
+let navbar=document.querySelector('.navbar');
+
+menu.onclick=()=>{
+    navbar.classList.toggle('active');
+    cart.classList.remove('active');
+    login.classList.remove('active');
+    
+}
+
+// cart toggle
+let cart=document.querySelector('.cart');
+
+document.querySelector('#cart-icon').onclick=()=>{
+    cart.classList.toggle('active');
+    navbar.classList.remove('active');
+    menu.classList.remove('move');
+    login.classList.remove('active');
+}
+
+// login form toggle
+let login=document.querySelector('.login-form');
+
+document.querySelector('#user-icon').onclick=()=>{
+    login.classList.toggle('active');
+    cart.classList.remove('active');
+    navbar.classList.remove('active');
+    menu.classList.remove('move');
+
+}
